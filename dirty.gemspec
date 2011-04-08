3e656e323c5c7cfca83e099b2b728cfb3b8c8c76
Robert Pitts <rbxbxdev@gmail.com>
2011-04-08 10:29:30 -0400

    Remove dependency on grit for performance

--- dirty.gemspec
+++ dirty.gemspec
@@ -19,6 +19,4 @@ Gem::Specification.new do |s|
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
-
-  s.add_runtime_dependency 'grit', '~> 2.4.1'
 end
--- lib/dirty.rb
+++ lib/dirty.rb
@@ -1,36 +1,14 @@
-require 'grit'
-
 class Dirty
-  def repo
-    @repo ||= Grit::Repo.new(dir)
-  end
-
-  def changed_files
-    files(:changed)
-  end
-
-  def added_files
-    files(:added)
-  end
-
-  def untracked_files
-    files(:untracked)
-  end
-
-  def files(type)
-    repo.status.send(type).map(&:first)
+  def status
+    `git status --porcelain`.split("\n")
   end
 
   def dirty_files
-    changed_files | added_files | untracked_files
+    status.reject { |f| f[/^ D/] }
   end
 
   def dirty_features
-    dirty_files.select { |f| f.split('.').last[/feature/] }
-  end
-
-  def dir
-    File.expand_path('.')
+    dirty_files.map { |s| s[/(features.*\.feature)/] }
   end
 
   def self.run
