b6b30039af450ee97f1bc212496a918d777273ea
Jon Larkowski <jonlarkowski@gmail.com>
2011-04-08 16:14:35 -0700

    Exit if no dirty features

--- lib/dirty.rb
+++ lib/dirty.rb
@@ -10,10 +10,14 @@ module Dirty
   end
 
   def dirty_features
-    dirty_files.map { |s| s[/(features.*\.feature)/] }
+    dirty_files.map do |s|
+      s[/(features.*\.feature)/]
+    end.compact
   end
 
   def perform
+    return 0 if dirty_features.empty?
+
     system("cucumber #{dirty_features.join(' ')}") if dirty_features.any?
 
     # Return cucumber's error status.
