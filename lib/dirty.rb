a4f7f7b17a63c9e3e778eb8f15bedbf7e565ed18
Robert Pitts <rbxbxdev@gmail.com>
2011-04-19 19:56:41 -0400

    Add support for rspec

--- lib/dirty.rb
+++ lib/dirty.rb
@@ -1,29 +1,58 @@
 module Dirty
   extend self
 
-  def status
-    `git status --porcelain`.split("\n")
+  def perform
+    commands.any? ? run_commands : 0
+  end
+
+  def run_commands
+    commands.each { |c| puts(c); system(c) }
+    $?.exitstatus
+  end
+
+  def commands
+    case type
+    when /feature/
+      [cucumber]
+    when /spec/
+      [rspec]
+    else
+      [rspec, cucumber]
+    end.compact
+  end
+
+  def type
+    @type ||= ARGV[0]
+  end
+
+  def rspec
+    "rspec #{dirty_specs.join(' ')}" if dirty_specs.any?
+  end
+
+  def cucumber
+    "cucumber #{dirty_features.join(' ')}" if dirty_features.any?
+  end
+
+  def dirty_features
+    dirty_matches(/(features.*\.feature)/)
+  end
+
+  def dirty_specs
+    dirty_matches(/(spec.*\_spec.rb)/)
+  end
+
+  def dirty_matches(regex)
+    dirty_files.map do |s|
+      s[regex]
+    end.compact
   end
 
   def dirty_files
     status.reject { |f| f[/^ D/] }
   end
 
-  def dirty_features
-    dirty_files.map do |s|
-      s[/(features.*\.feature)/]
-    end.compact
-  end
-
-  def perform
-    return 0 if dirty_features.empty?
-
-    command = "cucumber #{dirty_features * ' '}"
-    puts command
-    system command
-
-    # Return cucumber's error status.
-    $?.exitstatus
+  def status
+    `git status --porcelain`.split("\n")
   end
 
 end
